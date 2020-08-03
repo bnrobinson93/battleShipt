0125903b928efc96b56ee8bd30264099355b50ff
Brad R <bnrobinson93@gmail.com>
2020-08-03 12:47:45 -0500

    Removed depricated dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import '../node_modules/materialize-css/dist/css/materialize.css';
-import './index.css';
 import ReactDOM from 'react-dom';
 import BattleShipt from './battleShipt.js';
+import './index.css';
 
 ReactDOM.render(<BattleShipt />, document.getElementById('root'));
